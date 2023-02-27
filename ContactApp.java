2901315a87de7d036ca6fb1e27326ca2cc50c6b7
Horace Smith <horacesmith26@gmail.com>
2023-02-27 10:59:20 -0600

    added get info method

---
+++ ContactApp.java
@@ -0,0 +1,7 @@
+public class ContactApp {
+    public static void main(String[] args) {
+        contact c1 = new contact("jimbo jones","00000009999999");
+        System.out.println(c1.getInfo());
+    }
+
+}
--- contact.java
+++ contact.java
@@ -2,28 +2,41 @@ import java.util.HashMap;
 
 public class contact {
     private String name;
-    private int number;
+    private String number;
 //    private HashMap<String, Integer> list;
 
-    public contact(String name, int number){
+    public contact(String name, String number){
         this.name = name;
-        this.number = number;
+        this.number = formatNumber(number);
     }
 
+    public String formatNumber(String num){
+        String fnumber = num;
+        if (num.length()== 7){
+            fnumber = num.replaceFirst("(\\d{3})(\\d+)", "$1-$2");
+        } else if (num.length()==10){
+            fnumber = num.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
+        } else if (num.length() > 10){
+//
+            fnumber ="+" + num.substring(0,num.length()-10) + " " + num.substring(num.length()-10).replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
+        } else if (num.length() > 13){
+           fnumber = "sorry this number input is incorrect";
+        }
+        return fnumber;
+    };
 
-
-    public HashMap<String, Integer> getList() {
-        HashMap<String, Integer> list = new HashMap<String, Integer>() {
-            {
-                put(name, number);
-            }
-
-        };
-        return list;
+    public String getInfo() {
+        return name + number;
     }
-    public static void main(String[] args) {
 
-    }
-
+    //    public HashMap<String, String> getList() {
+//        HashMap<String, String> list = new HashMap<>() {
+//            {
+//                put(name, formatNumber());
+//            }
+//
+//        };
+//        return list;
+//    }
 }
 
