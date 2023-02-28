f32dc391df3fe47ea6db315235f1380d5d052089
Jeffrey Neal <jeffneal11@gmail.com>
2023-02-28 11:45:33 -0600

    add name/num validation, prettied table, cleaned comments

--- src/ContactApp.java
+++ src/ContactApp.java
@@ -1,69 +1,40 @@
 package src;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
 
-
-public class ContactApp {
-
-    /**
-     * TODO:
-     * add checks to user input ;; disallow diamonds
-     */
-    protected static final Scanner scanner = new Scanner(System.in);
-    //  protected static final HashMap<String, String> contacts = new HashMap<>();
+public class ContactApp implements colors{
+    private static final Scanner scanner = new Scanner(System.in);
     protected static final List<Contact> contacts = new ArrayList<>();
-
-
-    private static final String fileName = "contacts.txt";
-    private static final Path filePath = Paths.get("src", fileName);
-
+    private static final Path filePath = Paths.get("src", "contacts.txt");
     public static void main(String[] args) {
         validateFile();
         loadContacts();
         boolean running = true;
 
         while (running) {
-            System.out.printf("1. View contacts.\n2. Add a new contact.\n3. Search a contact by name.\n4. Delete an existing contact.\n5. Exit.\nEnter an option (1, 2, 3, 4 or 5):\n");
+            System.out.printf("%s1. View contacts.\n2. Add a new contact.\n3. Search a contact by name.\n4. Delete an existing contact.\n5. Exit.%s\nEnter an option (1, 2, 3, 4 or 5):\n",WHITE_BRIGHT,RESET);
 
             int ui = scanner.nextInt();
             scanner.nextLine();
             switch (ui) {
-                case 1:
-//                System.out.println(ui);
-                    displayContacts();
-                    break;
-                case 2:
-//                System.out.println(ui);
-                    addContacts();
-                    break;
-                case 3:
-                    searchContact();
-                    break;
-                case 4:
-                    deleteContact();
-                    break;
-                case 5:
-//                    System.out.println(ui);
-                    running = false;
-                    break;
-                default:
-                    System.out.println("System Error");
-                    break;
+                case 1 -> displayContacts();
+                case 2 -> addContacts();
+                case 3 -> searchContact();
+                case 4 -> deleteContact();
+                case 5 -> running = false;
+                default -> System.out.println("Please enter a valid menu option");
             }
             if (running) {
-                System.out.println("continue...? (y/n)");
+                System.out.printf("%scontinue...? (%sy%s/%sn%s)%s%n",WHITE_BRIGHT,GREEN_BRIGHT,WHITE_BRIGHT,RED_BRIGHT,WHITE_BRIGHT,RESET);
                 if (scanner.nextLine().equalsIgnoreCase("n")) {
                     running = false;
                 }
             }
-            if (!running) System.out.println("Have a nice day");
+            if (!running) System.out.printf("%s%sHave a nice day! %s%n",ITALICS,CYAN_BOLD_BRIGHT,SHRIMP);
         }
-
-
-//        contacts.put(get,"7028888888");
-//        System.out.println(contacts);
     }
 
     public  static String formatNumber(String num) {
@@ -73,7 +44,6 @@ public class ContactApp {
         } else if (num.length() == 10) {
             fnumber = num.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
         } else if (num.length() > 10) {
-
             fnumber = "+" + num.substring(0, num.length() - 10) + " " + num.substring(num.length() - 10).replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
         }
         return fnumber;
@@ -81,12 +51,11 @@ public class ContactApp {
 
     public static void deleteContact(){
         Contact toRemove = null;
-        System.out.println("Enter name of contact to delete");
+        System.out.printf("%sEnter name of contact to delete%s%n",RED,RESET);
         String isName = scanner.nextLine();
         for (Contact c : contacts) {
             if (c.getName().equalsIgnoreCase(isName)){
              toRemove = c;
-
             }
         }
         if (toRemove == null){
@@ -96,8 +65,7 @@ public class ContactApp {
             System.out.println("contact deleted");
             saveContacts();
         }
-
-    };
+    }
 
     public static void searchContact(){
         boolean isSuccessful = false;
@@ -105,20 +73,19 @@ public class ContactApp {
         String theName = scanner.nextLine();
         for (Contact c : contacts) {
             if (c.getName().equalsIgnoreCase(theName)){
-                System.out.println(c.getName() + " " + formatNumber(c.getNumber()));
+                System.out.printf("%s%s%s - %s%s%s%n",PURPLE_BRIGHT,c.getName(),WHITE_BRIGHT,PURPLE_BRIGHT,  formatNumber(c.getNumber()),RESET);
                 isSuccessful = true;
             }
         }
         if (!isSuccessful) System.out.println("Invalid name");
-
-    };
+    }
 
     public static void displayContacts() {
-        contactFormatter();
+        if(contacts.size() >= 1)contactFormatter();
+        else System.out.println("No contacts found...");
     }
 
     private static void validateFile() {
-//        System.out.println("ddd");
         if (!Files.exists(filePath)) {
             try {
                 Files.createFile(filePath);
@@ -129,7 +96,6 @@ public class ContactApp {
     }
 
     public static void loadContacts() {
-//        System.out.println();
         contacts.clear();
         List<String> newList = new ArrayList<>();
         try {
@@ -137,7 +103,6 @@ public class ContactApp {
         } catch (IOException iox) {
             iox.printStackTrace();
         }
-//        System.out.println(newList);
         if (newList.size() >= 1) {
             for (String s : newList) {
                 String[] arr = s.split("◈");
@@ -146,7 +111,6 @@ public class ContactApp {
             }
         }
     }
-
 
     public static void saveContacts() {
         List<String> contactString = new ArrayList<>();
@@ -160,35 +124,75 @@ public class ContactApp {
             System.out.println(e);
         }
     }
-
-    public static void addContacts() {
-        System.out.println("Enter Name...");
-        String uName = scanner.nextLine();
-        System.out.println("Enter Number...");
-        String uNumber = scanner.nextLine();
-        if (uName.length() > 0 && uNumber.length() > 6 && uNumber.length() <= 14){
-            Contact c = new Contact(uName, uNumber);
-            contacts.add(c);
-
-            saveContacts();
-        } else {
-            System.out.println("Please enter a valid input");
+    public static boolean validatePhoneNumber(String str)
+    {
+        if(str.length() < 7 || str.length() > 13)
+            return false;
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i)))
+                return false;
         }
+        return true;
+    }
+    public static boolean validateName(String str)
+    {
+        if (str.length() == 0 || str.length() > 20)
+            return false;
 
-
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '◈')
+                return false;
+        }
+        return true;
+    }
+    public static void addContacts() {
+        String outName = null;
+        String outNumber = null;
+        while (outName == null) {
+            System.out.println("Enter Name...");
+            String uName = scanner.nextLine();
+            if (!validateName(uName)) {
+                System.out.println("Please enter a valid name");
+            } else {
+                outName = uName;
+            }
+        }
+        while (outNumber == null){
+            System.out.println("Enter Number...");
+            String uNumber = scanner.nextLine();
+            if (!validatePhoneNumber(uNumber)) {
+                System.out.println("Please enter a valid phone number");
+            }else{
+                outNumber = uNumber;
+            }
+        }
+        boolean passable = true;
+        for(Contact c : contacts){
+            if(c.getName().equalsIgnoreCase(outName)){
+                passable = false;
+                System.out.println("Name already exists in contacts! please try again!");
+            }
+            if(c.getNumber().equals(outNumber)){
+                passable = false;
+                System.out.println("Number already exists in contacts! please try again!");
+            }
+        }
+        if(passable) {
+            Contact c = new Contact(outName, outNumber);
+            System.out.printf("%sContact for %s added!%s%n", GREEN_BRIGHT, outName, RESET);
+            contacts.add(c);
+            saveContacts();
+        }
     }
 
     public static void contactFormatter() {
-        System.out.println("------------------------------------");
-        System.out.printf("|%-15s|%-18s|%n", "Name","Phone Number");
-        System.out.println("------------------------------------");
+        System.out.println("┌───────────────┬───────────────────┐");
+        System.out.printf("│%-15s│%-19s│%n", "Name","Phone Number");
+        System.out.println("┝━━━━━━━━━━━━━━━┿━━━━━━━━━━━━━━━━━━━┥");
         for (Contact c : contacts) {
-            System.out.printf("|%-15s|%-18s|%n", c.getName(),formatNumber(c.getNumber()));
+            System.out.printf("│%s%-15s%s│%s%-19s%s│%n", YELLOW,c.getName(),RESET,YELLOW,formatNumber(c.getNumber()),RESET);
         }
-        ;
-        System.out.println("------------------------------------");
-
-
+        System.out.println("└───────────────┴───────────────────┘");
     }
 
 }
